ebf52af87498ff352eafbc46397440ed04401201
Roqhaye Halimi <roqiahalimi95@gmail.com>
2023-12-17 14:56:14 -0800

    create save and delete method

--- javascript.js
+++ javascript.js
@@ -21,16 +21,34 @@ const notes = [
 
 class NotesAPI {
   static getAllNotes() {
-    const savedNotes = notes || [];
+    const savedNotes = JSON.parse(localStorage.getItem("notes-app")) || [];
     return savedNotes.sort((a, b) => {
       return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
     });
   }
   //JSON.parse(localStorage.getItem("notes-app"))
-  static saveNote() {
+  static saveNote(noteToSave) {
     //1. existes or 2. not
+    const notes = NotesAPI.getAllNotes();
+
+    const existedNote = notes.find((n) => n.id == noteToSave.id);
+
+    if (existedNote) {
+      existedNote.title = noteToSave.title;
+      existedNote.body = noteToSave.body;
+      existedNote.updated = new Date().toISOString();
+    } else {
+      noteToSave.id = new Date().getTime();
+      noteToSave.updated = new Date().toISOString();
+      notes.push(noteToSave);
+    }
+    localStorage.setItem("notes-app", JSON.stringify(notes));
   }
 
-  deleteNote() {}
+  static deleteNote(id) {
+    const notes = NotesAPI.getAllNotes();
+    const filterNotes = notes.filter((n) => n.id != id);
+    localStorage.setItem("notes-app", JSON.stringify(filterNotes));
+  }
 }
-console.log(NotesAPI.getAllNotes());
+console.log(NotesAPI.deleteNote(3));
